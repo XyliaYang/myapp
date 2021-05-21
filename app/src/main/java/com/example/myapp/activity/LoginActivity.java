ce4d8bbf02756b11bc55421c9dcbcfd6bf78a6ff
xyliayang <781454061@qq.com>
2021-05-21 10:59:17 +0800

    登录接口测试成功

--- app/src/main/java/com/example/myapp/activity/LoginActivity.java
+++ app/src/main/java/com/example/myapp/activity/LoginActivity.java
@@ -3,12 +3,29 @@ package com.example.myapp.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.myapp.R;
+import com.example.myapp.util.AppConfig;
 import com.example.myapp.util.StringUtils;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class LoginActivity extends BaseActivity {
 
@@ -38,6 +55,7 @@ public class LoginActivity extends BaseActivity {
     }
 
     private void loginIn(String user, String pwd) {
+
         if(StringUtils.isEmpty(user)){
             showToast("请输入用户名");
             return;
@@ -46,6 +64,29 @@ public class LoginActivity extends BaseActivity {
             showToast("请输入密码");
             return;
         }
+
+        //第一步获取okHttpClient对象
+        OkHttpClient client = new OkHttpClient.Builder()
+                .build();
+        //第二步构建Request对象
+        Request request = new Request.Builder()
+                .url(AppConfig.BASE_URL)
+                .get()
+                .build();
+        //第三步构建Call对象
+        Call call = client.newCall(request);
+        //第四步:异步get请求
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.i("onFailure", e.getMessage());
+            }
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String result = response.body().string();
+                Log.i("result", result);
+            }
+        });
 
 
     }
---
+++ server/app/login.py
@@ -0,0 +1,6 @@
+{
+"msg": "success",
+"code": 0,
+"expire": 604800,
+"token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiaWF0IjoxNTkyMDQ0MzEyLCJleHAiOjE1OTI2NDkxMTJ9.FpOxHrkpYSexqo91Uk8YiS1Nf1Plox4a4nYoFu6ArLcy28beZyoGJbf9fTJ3L-eUs2ku0zABoxjDuMAZ7qzNhw"
+}
